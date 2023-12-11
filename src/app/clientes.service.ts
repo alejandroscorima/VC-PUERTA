f8ff3611aae6d40b14b0a01679de994a7e2ba2aa
martinoscorima <a20172445@pucp.edu.pe>
2023-12-11 10:44:57 -0500

    change API url

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -51,7 +51,7 @@ export class ClientesService {
 
   getClientFromReniec(doc_number: string) {
 
-    this.urlconsulta = 'https://my.apidevs.pro/api/dni/'+doc_number+'?api_token=e9cc47e67d492cdee675bfb2b365c09393611b5141144aa0da34cab5429bb5e8';
+    this.urlconsulta = 'https://my.apidev.pro/api/dni/'+doc_number+'?api_token=e9cc47e67d492cdee675bfb2b365c09393611b5141144aa0da34cab5429bb5e8';
     return this.http.get(this.urlconsulta);
 
   }
